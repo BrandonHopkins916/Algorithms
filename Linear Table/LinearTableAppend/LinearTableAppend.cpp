00b8bd6122eb67b12f6c861617b7ba7cabce355d
Brandon Hopkins <brandonhopkins320@gmail.com>
2023-04-22 15:20:46 -0700

    Add Linear Table Append
    Add Linear Table Append with delete function

---
+++ Linear Table/LinearTableAppend/LinearTableAppend.cpp
@@ -0,0 +1,34 @@
+// LinearTableAppend.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+int main()
+{
+    int array[] = { 90, 70, 50, 80, 60, 85 };
+
+    int length = ( sizeof(array) / sizeof(array[0]) );
+
+    int* tempArray = new int[length + 1];
+
+    for (int i = 0; i < length; i++)
+    {
+        tempArray[i] = array[i];
+    }
+
+    tempArray[length] = 75;
+
+    memcpy_s( tempArray, sizeof(tempArray), array, sizeof(tempArray) );
+
+    for(int j = 0; j < length + 1; j++)
+    {
+        printf("%d\n", tempArray[j]);
+    }
+
+    printf("Releasing tempArray\n");
+    delete[] tempArray;
+    printf("Release tempArray\n");
+
+    return 0;
+}
